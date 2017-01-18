4d44e0e883305feb31241985e86890f6a06f570a
Matthew <mattmuelle@gmail.com>
2017-01-18 13:36:18 +0700

    make poss work in browsers

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
-'use strict'
+'use strict';
 
 /**
  * Module Dependencies
  */
 
-const co = require('co')
+var regeneratorRuntime = require('babel-runtime/regenerator')
+var _marked = [poss].map(regeneratorRuntime.mark);
+var co = require('co');
 
 /**
  * Export `poss`
  */
 
-module.exports = co.wrap(poss)
+module.exports = co.wrap(poss);
 
 /**
  * Poss
@@ -19,10 +21,33 @@ module.exports = co.wrap(poss)
  * @return {Mixed}
  */
 
-function * poss (gen) {
-  var o
-  try { o = yield gen }
-  catch (e) { return [e, null] }
-  return [null, o]
-}
+function poss(gen) {
+  var o;
+  return regeneratorRuntime.wrap(function poss$(_context) {
+    while (1) {
+      switch (_context.prev = _context.next) {
+        case 0:
+          _context.prev = 0;
+          _context.next = 3;
+          return gen;
 
+        case 3:
+          o = _context.sent;
+          _context.next = 9;
+          break;
+
+        case 6:
+          _context.prev = 6;
+          _context.t0 = _context['catch'](0);
+          return _context.abrupt('return', [_context.t0, null]);
+
+        case 9:
+          return _context.abrupt('return', [null, o]);
+
+        case 10:
+        case 'end':
+          return _context.stop();
+      }
+    }
+  }, _marked[0], this, [[0, 6]]);
+}
